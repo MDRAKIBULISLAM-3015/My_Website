0ffbad10791619a20d5a538f4eb255dbabfcbb44
MD RAKIBUL ISLAM <rakibul15-3015@diu.edu.bd>
2022-11-03 19:32:08 +0600

    update

--- Script.js
+++ Script.js
@@ -43,30 +43,17 @@ function closemenu() {
   document.getElementById("sidemenu").classList.remove("classnam");
 }
 
-$(document).ready(function ($) {
-  $("body").scrollToTop({
-    distance: 200,
-    speed: 1000,
-    easing: "linear",
-    animation: "fade", // fade, slide, none
-    animationSpeed: 500,
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 600) {
+    $(".scrollup").fadeIn();
+  } else {
+    $(".scrollup").fadeOut();
+  }
+})
 
-    mobile: {
-      width: 768,
-      distance: 100,
-      speed: 1000,
-      easing: "easeInOutElastic",
-      animation: "slide",
-      animationSpeed: 200,
-    },
-
-    trigger: null, // Set a custom triggering element. Can be an HTML string or jQuery object
-    target: null, // Set a custom target element for scrolling to. Can be element or number
-    text: "Scroll To Top", // Text for element, can contain HTML
-
-    skin: null,
-    throttle: 250,
-
-    namespace: "scrollToTop",
-  });
-});
+$(".scrollup").click(function() {
+  $("html, body").animate({
+    scrollTop: 0
+  }, 600);
+  return false;
+})
